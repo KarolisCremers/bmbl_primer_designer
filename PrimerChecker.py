cf70f697374668279e2256d89a1672fc27ffb083
EngineerCoding <EngineerCoding@hotmail.nl>
2017-01-18 23:59:30 +0100

    Document base OOP classes

--- PrimerChecker.py
+++ PrimerChecker.py
@@ -5,10 +5,37 @@ _check_methods = "dimer", "hairpin", "self_dimer"
 
 
 class PrimerChecker(object):
+    """ The PrimerChecker object will check dimers, self dimers and
+    hairpins determined by the parameters in the constructor. These
+    are all experimental since it is string based and DOES NOT
+    CALCULATE the Gibbs free energy which usually is done.
+    Every method depends is_dimer, however when the dimer check is
+    disabled this will not effect the result of self dimers or
+    hairpins. This is done through the python magic method
+    `__getattribute__` and an internal variable called
+    `_internal_override`
+    This class also contains utility methods to check primers, for
+    instance `calc_primer_details`
+    """
+
     complement_dict = dict(A='T', T='A', C='G', G='C')
 
     def __init__(self, required_bonds=3, dimer=True, self_dimer=True,
                  hairpin=True):
+        """ This method will configure whether methods are enabled
+        (True) or disabled (False). When a method is disabled, the
+        method will always return True to ensure all checks pass
+        and the using code has not to worry about this configuration.
+
+        Parameters:
+            required_bonds - The amount of bonds that are required to
+            invalidate a dimer, self dimer or hairpin.
+            dimer - Enables or disables `is_dimer`
+            self_dimer - Enables or disables `is_self_dimer`
+            hairpin -  - Enables or disables `hairpin`
+        Returns:
+            -
+        """
         self.required_bonds = required_bonds
         self.check_dimer = dimer
         self.check_self_dimer = self_dimer
@@ -16,6 +43,17 @@ class PrimerChecker(object):
         self._internal_override = False
 
     def check_bonds(self, primer_a, primer_b):
+        """ Checks whether the two sequences can are bound together and
+        are strong enough to actually invalidate the sequences. This
+        means that required_bonds from the constructor is used.
+
+        Parameters:
+            primer_a - A sequence WITHOUT spaces
+            primer_b - A sequence with or without spaces
+        Returns:
+            True when the sequences should be invalidated and False
+            when it is valid
+        """
         bonds = 0
         for i in range(len(primer_a)):
             if self.complement_dict[primer_a[i]] == primer_b[i]:
@@ -25,9 +63,16 @@ class PrimerChecker(object):
         return False
 
     def calc_primer_details(self, primer):
-        ''' Calculates the melting temperature and GC% of a primer which
+        """ Calculates the melting temperature and GC% of a primer which
         has a length less than 25 nucleotides.
-        '''
+
+        Parameters:
+            primer - The primer to check for
+        Returns:
+            1. The GC percentage
+            2. The melting temperature
+            This is a tuple.
+        """
         gc_length = len(re.findall('[GC]', primer))
         # Calculate the GC%
         gc_perc = (float(gc_length) / len(primer)) * 100
@@ -36,6 +81,22 @@ class PrimerChecker(object):
         return gc_perc, melting_temp
 
     def is_dimer(self, primer_a, primer_b):
+        """ Checks whether primer_a and primer_b can form a dimer based
+        on the required_bonds setting in the constructor. The largest
+        primer always should not have spaces embedded in the string,
+        otherwise KeyErrors will be thrown. In the case of equal length
+        primers, the first primer (primer_a) is picked to be the "large"
+        primer. This behaviour is used in `is_hairpin`
+        This method will move the small primer along the large primer,
+        so every possibility is accounted for.
+
+        Parameters:
+            primer_a - A primer containing no spaces
+            primer_b - A primer of any length which may contain primers
+        Returns:
+            Whether the two primers can form a dimer (True) or not
+            (False).
+        """
         # Assume primers dont have whitespace and are uppercase
         large_primer = primer_a if len(primer_a) >= len(primer_b) else primer_b
         small_primer = primer_b if large_primer == primer_a else primer_a
@@ -52,12 +113,31 @@ class PrimerChecker(object):
         return False
 
     def is_self_dimer(self, primer):
+        """ This will check whether the primer can form a dimer with
+        another primer of the same type. This means that a check is
+        made whether the forward primer can form a dimer with another
+        forward primer. This is also goes for the reverse primer with
+        the reverse primer.
+
+        Parameters:
+            primer - The primer to do the self dimer check on
+        Returns:
+            Whether the primer can form a self dimer (True) or not
+            (False).
+        """
         self._internal_override = True
         output = self.is_dimer(primer, primer, required_bonds)
         self._internal_override = False
         return output
 
     def is_hairpin(self, primer):
+        """ Checks whether the primer can form a hairpin.
+
+        Parameters:
+            primer - The primer to do the hairpin check on
+        Returns:
+            Whether the primer can form a hairpin (True) or not (False)
+        """
         if len(primer) < self.required_bonds * 2:
             return False
         self._internal_override = True
@@ -74,9 +154,26 @@ class PrimerChecker(object):
         return False
 
     def __getattribute__(self, key):
+        """ This is a 'magic method' in python and should not be called
+        directly unless a child class needs the super form.
+        Refer for this method to: http://bit.ly/2ja7BKM
+        This method handles whether a method should be called in the
+        first place defined by the dimer, self_dimer and hairpin
+        settings in the constructor.
+        This is done because the method `is_dimer` is both used by
+        `is_self_dimer` and `is_hairpin.`
+        This check is done when only when the variable
+        `_internal_override` is False and the configuration variable
+        is also False.
+
+        Parameters:
+            key - The attribute which needs to be returned
+        Returns:
+            The correct attribute, or true when a check should not be
+            done.
+        """
         if key[3:] in _check_methods:
-            print("Override: " + str(self._internal_override))
             if (not self._internal_override and
                     not getattr(self, "check_" + key[3:])):
-                return lambda *x: False
+                return lambda *x: True
         return super(PrimerChecker, self).__getattribute__(key)
--- PrimerFinder.py
+++ PrimerFinder.py
@@ -2,9 +2,36 @@ from re import split
 
 
 class PrimerFinder(object):
+    """ This is a baseclass which handles the settings of a complete
+    input from the input and modifies the sequence to make sure it
+    is upper case. This is already done in the GUI, but this is a
+    standalone class and should also do it.
+    The method `find_primers` should be considered as a main method.
+    This should return a list of results which look like the
+    following structure:
+    TODO
+    """
 
     def __init__(self, primer_checker, sequence, anneal_minimum,
                  anneal_maximum, max_pcr_product):
+        """ The constructor of this class which initialises the
+        input variables. Only the sequence is converted to uppercase
+        and removes all whitespace from it.
+
+        Parameters:
+            primer_checker - A PrimerChecker object which handles the
+            checks for dimers, self dimers and hairpins (experimentally)
+            sequence - The sequence of nucleotides where primers need to
+            be found in.
+            anneal_minimim - Determines the minimum of the range of the
+            actual sequence which primers are allowed to anneal to.
+            anneal_maximum - Determines the maximum of the range of the
+            actual sequence which primers are allowed to anneal to.
+            max_pcr_product - Determines the length of the maximal
+            final PCR product.
+        Returns:
+            -
+        """
         self.primer_checker = primer_checker
         self.sequence = "".join(split("\s+", sequence.upper()))
         self.anneal_minimum = anneal_minimum
@@ -12,16 +39,35 @@ class PrimerFinder(object):
         self.max_pcr_product = max_pcr_product
 
     def complement_sequence(self, sequence, flip_sequence=True):
+        """ Creates the complement of a given sequence.
+
+        Parameters:
+            sequence - The sequence to make the complement of.
+            flip_sequence - Determines whether to make the complement
+            from 3' to 5' (True) or 5' to 3'.
+        Returns:
+            The complement of the given sequence in the correct
+            orientation determined by flip_sequence.
+        """
         complement_dict = dict(A='T', T='A', C='G', G='C')
         complement_seq = ""
         if flip_sequence:
-            sequence = reversed(seqeunce)
+            sequence = reversed(sequence)
         for nucleotide in sequence:
             complement_seq += complement_dict[nucleotide]
         return complement_seq
 
     def get_annealing_sequence(self):
+        """ Retrieves the sequence which primers can anneal to. Uses
+        anneal_minimum and anneal_maximum from the constructor.
+
+        Parameters:
+            -
+        Returns:
+            The sequence which primers can anneal to.
+        """
         return self.sequence[self.anneal_minimum - 1:self.anneal_maximum - 1]
 
     def find_primers(self):
+        """ The main method which searches for the primers for this class """
         raise NotImplementedError
